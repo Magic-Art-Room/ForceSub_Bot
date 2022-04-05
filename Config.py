488f9db0d01505a1fee4b4ec7525ce8e93de902e
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2022-04-05 21:49:14 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -21,9 +21,9 @@ class Messages():
 
         "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
         
-        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
+        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator/owner of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
         
-        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username or channel ID} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n/source_code - To get bot source code😍\n\nNote: /FSub is an alias of /ForceSubscribe__",
+        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username or channel ID} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n★ 👉All Above Commands Works in Group Only 👈\n\nNote: /FSub is an alias of /ForceSubscribe__",
         
        "**Devloped By @ARKBotz**"
       ]
